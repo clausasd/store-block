d2a51168b1b4da9812a826a4a335c12dac63cbdf
Claudiu Florea <claudiu.florea@outlook.com>
2020-09-21 11:15:39 +0300

    Making the countdown block customizable #3

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React from "react";
 
-interface CountdownProps { }
-
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ }) => {
-  return <div></div>
+interface CountdownProps {
+  targetDate: string;
 }
+
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
+  targetDate,
+}) => {
+  return (
+    <div>
+      <h1>{targetDate}</h1>
+    </div>
+  );
+};
 
 Countdown.schema = {
-  title: 'editor.countdown.title',
-  description: 'editor.countdown.description',
-  type: 'object',
+  title: "editor.countdown.title",
+  description: "editor.countdown.description",
+  type: "object",
   properties: {},
-}
+  targetDate: {
+    title: "Final date",
+    description: "Final date used in the countdown",
+    type: "string",
+    default: null,
+  },
+};
 
-export default Countdown
+export default Countdown;
